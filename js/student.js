204e1ef565eebf91009fb40cc3e79b6e8bf9fc26
Ranjit Bijoy <ranjit.bijoy@careerbuilder.com>
2015-04-07 14:42:53 -0400

    add two-tabbed pane

--- js/student.js
+++ js/student.js
@@ -1,31 +1,31 @@
 
-		var app = angular.module('student', []);
+var app = angular.module('student', []);
     	//app.controller('',);
 
 
     	app.controller('StudentPanelController', function(){
-   			this.tab = 0;
-   			this.setTab=function(tab){
-   				this.tab = tab;
-	   		};
-   			this.isSelected = function(tab){
-   				return this.tab === tab;
-   			};
-   		});
+        this.tab = 0;
+        this.setTab=function(tab){
+         this.tab = tab;
+       };
+       this.isSelected = function(tab){
+         return this.tab === tab;
+       };
+     });
     	app.directive('classPreferences', function(){
-   			return{
-   				restrict: 'E',
-   				templateUrl: 'student/class-preferences.html',
-          controller: ['$http', function($http){
-            var ctrl = this;
-            ctrl.stuff = [];
-            $http.get('/testjson/test.json').success(function(data){
-              ctrl.stuff = data;
-            });
-          }],
-          controllerAs: 'cpCtrl'
-   			};
-   		});
+        return{
+         restrict: 'E',
+         templateUrl: 'student/class-preferences.html',
+         controller: ['$http', function($http){
+          var ctrl = this;
+          ctrl.stuff = [];
+          $http.get('/testjson/test.json').success(function(data){
+            ctrl.stuff = data;
+          });
+        }],
+        controllerAs: 'cpCtrl'
+      };
+    });
 
       app.directive('recommendedCourses', function(){
         return{
@@ -47,16 +47,32 @@
           restrict: 'E',
           templateUrl: 'student/historical-data.html',
           controller: ['$http', function($http){
+            var tabClasses;
             var ctrl = this;
-            ctrl.stuff = [];
-            $http.get('/testjson/test.json').success(function(data){
-              ctrl.stuff = data;
-            });
+            function initTabs() {
+              tabClasses = ["",""];
+            }
+
+            ctrl.getTabClass = function (tabNum) {
+              return tabClasses[tabNum];
+            };
+
+            ctrl.getTabPaneClass = function (tabNum) {
+              return "tab-pane " + tabClasses[tabNum];
+            }
+
+            ctrl.setActiveTab = function (tabNum) {
+              initTabs();
+              tabClasses[tabNum] = "active";
+            };
+
+            initTabs();
+            ctrl.setActiveTab(1);
           }],
           controllerAs: 'hdCtrl'
         };
       });
 
 
-   
-    
+
+
